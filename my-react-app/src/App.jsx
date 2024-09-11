9c1ceb161c3752c894d5a71f2b7ef17c3b990397
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-11 15:37:50 -0300

    Iniciando em React

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+
+function App() {
+//um jeito mais pratico de escrever <header></header> é <Header/>
+    return(
+        <>
+        <Header/>
+        <Food/>
+        <Footer/>
+        </>
+    );
+}
+
+export default App
